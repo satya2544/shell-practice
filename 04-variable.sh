0107caa05218ec574dee8484aefe871d7eed06d1
satya2544 <koppolu.satya@gmail.com>
2026-01-06 13:54:31 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom"
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom"
+
