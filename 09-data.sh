462741905632fcabe1ab731079e7652e64502d7b
satya2544 <koppolu.satya@gmail.com>
2026-01-07 18:15:46 +0530

    variables

--- 09-data.sh
+++ 09-data.sh
@@ -2,8 +2,17 @@
 
 NUMBER1=100
 NUMBER2=200
-
+NAME=DevOps
 
 SUM=$(($NUMBER1+$NUMBER2))
 
+
+
 echo "SUM is: ${SUM}"
+
+
+Leaders=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${Leaders[@]}"
+echo "First Leader: ${Leaders[0]}"
+echo "First Leader: ${Leaders[10]}"
