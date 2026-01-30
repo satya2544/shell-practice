211a52600cdd0785609d5e8b177602c5d33edd4a
satya2544 <koppolu.satya@gmail.com>
2026-01-30 14:48:30 +0000

    variables

--- 20-installation.sh
+++ 20-installation.sh
@@ -2,6 +2,12 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
 if [ $USERID -ne 0 ]; then
      echo "ERROR:: Please run this script with root privelege"
      exit 1 # failure is other than 0
@@ -9,24 +15,40 @@ fi
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-     echo "ERROR:: Installing $2 is failure"
+     echo -e "ERROR:: Installing $2 ... $R is failure $N"
      exit 1
    else
-     echo "Installing $2 is SUCCESS"
+     echo -e "Installing $2 ... $G is SUCCESS $N"
    fi
 }
 
-dnf install mysql -y
-VALIDATE $? "MySQL"
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+     dnf install mysql -y
+     VALIDATE $? "MySQL"
+else
+    echo "MySQL already exist ... $G SKIPPING $N"
+fi
 
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
+dnf list installed nginx
 
 
-dnf install python3 -y
-VALIDATE $? "python3"
+if [ $? -ne 0 ]; then
+   dnf install nginx -y
+   VALIDATE $? "Nginx"
+else
+    echo "Nginx already exist ... $G SKIPPING $N"
+fi
 
+
+if [ $? -ne 0 ]; then
+   dnf install python3 -y
+   VALIDATE $? "python3"
+else
+    echo "python already exist ... $G SKIPPING $N"
+fi
 
 
 
