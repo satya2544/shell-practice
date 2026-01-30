1e78ed0df96573015b8ecfd9d41f64f77e49d60b
satya2544 <koppolu.satya@gmail.com>
2026-01-30 15:47:31 +0000

    variables

---
+++ 22-logs.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+LOGS_FOLDER = "/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)"
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other than 0
+fi
+
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+     echo -e "ERROR:: Installing $2 ... $R is failure $N"
+     exit 1
+   else
+     echo -e "Installing $2 ... $G is SUCCESS $N"
+   fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+
+if [ $? -ne 0 ]; then
+     dnf install mysql -y &>>$LOG_FILE
+     VALIDATE $? "MySQL"
+else
+    echo "MySQL already exist ... $G SKIPPING $N"
+fi
+
+
+dnf list installed nginx &>>$LOG_FILE
+
+
+if [ $? -ne 0 ]; then
+   dnf install nginx -y &>>$LOG_FILE
+   VALIDATE $? "Nginx"
+else
+    echo "Nginx already exist ... $G SKIPPING $N"
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+
+if [ $? -ne 0 ]; then
+   dnf install python3 -y &>>$LOG_FILE
+   VALIDATE $? "python3"
+else
+    echo "python already exist ... $G SKIPPING $N"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
