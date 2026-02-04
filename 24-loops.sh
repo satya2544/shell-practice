89b0b33198f9c241dc5a7dedbbc5bdc16c39ec37
satya2544 <koppolu.satya@gmail.com>
2026-02-04 17:09:32 +0000

    session-06

---
+++ 24-loops.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+LOGS_FOLDER = "/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other than 0
+fi
+
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+     echo -e "ERROR:: Installing $2 ... $R is failure $N" | tee -a $LOG_FILE
+     exit 1
+   else
+     echo -e "Installing $2 ... $G is SUCCESS $N" | tee -a $LOG_FILE
+   fi
+}
+
+
+
+for package in $@
+do
+    echo "Package is: $package"
+done
+
+
+
+
+
